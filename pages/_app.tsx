b27bcc8926cdd22772f6e53df75f35872072e4e1
Szymon Adamowicz <adamowiczszymon8@gmail.com>
2024-10-21 17:49:53 +0200

    scrolling bug after refresh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
-import "../global.css"
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import "../global.css";
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      window.scrollTo(0, 0);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
 
